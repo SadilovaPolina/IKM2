b31d03f3c76f7eb0947e26fa9e7cbe8d0ed24846
Полина Садилова <sadilovaPon06@gmail.com>
2025-06-13 19:23:48 +0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -5,32 +5,41 @@
 
 using namespace std;
 
+// Класс узла связного списка
 class ListNode {
 public:
-    string word;
+    string word; // Слово, хранящееся в узле
     ListNode* next;
 
     ListNode(const string& w);
 };
 
+// Класс для работы с цепочкой слов
 class ListChain {
 private:
     ListNode* head;
-    vector<string> words;
+    vector<string> words; // Вектор всех слов для построения цепочки
     vector<bool> used;
     vector<string> result;
 
+// Получить последнюю букву слова
     char getLast(const string& word);
+// Рекурсивная функция для поиска цепочки
     bool findChain(int step);
 
 public:
+// Конструктор, принимающий список слов
     ListChain(const vector<string>& wordList);
     ~ListChain();
+// Построить цепочку слов
     bool buildChain();
+// Вывести результат в файл
     void print(const string& filename);
     const vector<string>& Result() const;
 };
 
+// Функция для разделения строки на слова
 vector<string> split(const string& input);
+// Функция для сохранения строки в файл
 void saveToFile(const string& filename, const string& input);
 
